d9c16ed584ef041ed583de69c7648f83b3be0a88
Viktor Dzundza <vdzundza@protonmail.com>
2021-02-04 02:41:00 +0200

    feat(): add custom marker
    Signed-off-by: Viktor Dzundza <vdzundza@protonmail.com>

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,26 +1,40 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
+import GoogleIcon from "./../assets/icon.png";
 
-export class MapContainer extends Component<any> {
+const shawBar = {
+  lat: 50.4449,
+  lng: 30.4923,
+};
+export class MapContainer extends Component<any, any> {
   static defaultProps = {
-    center: {
-      lat: 50.44494579226836,
-      lng: 30.492384025297348,
-    },
-    zoom: 16,
+    center: shawBar,
+    zoom: 18,
   };
   constructor(props: any) {
     super(props);
   }
   render() {
     return (
-      <div style={{ height: "100vh", width: "100%" }}>
+      <div style={{ height: "500px", width: "100%" }}>
         <GoogleMapReact
           bootstrapURLKeys={{
             key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
           }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
+          options={{
+            disableDefaultUI: true,
+            draggable: false,
+            fullscreenControl: false,
+          }}
+          onGoogleApiLoaded={({ map, maps }) => {
+            new maps.Marker({
+              position: shawBar,
+              map,
+              icon: GoogleIcon,
+            });
+          }}
         ></GoogleMapReact>
       </div>
     );
